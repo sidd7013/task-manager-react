69c50880d45441c3d21b0d704d8e85e1dc630aa9
sidd7013 <sinkarsiddhesh06@gmail.com>
2026-04-09 13:08:23 +0530

    fix api url

--- src/services/taskService.js
+++ src/services/taskService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/tasks";
+const API_URL = "https://task-manager-backend-uq7s.onrender.com/tasks";
 
 export const getTasks =()=>{
     return axios.get(API_URL);
